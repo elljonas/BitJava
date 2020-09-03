078eb2d0b991fbd1dc20ede35394a2459c4701a0
elljonas <j.ellikas@gmail.com>
2020-09-03 15:19:31 +0300

    Adding changed project files

--- src/lt/jonas/app/HelloWorld.java
+++ src/lt/jonas/app/HelloWorld.java
@@ -3,5 +3,7 @@ package lt.jonas.app;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("Labas, Pasauli!!");
     }
+    // labas labas labas
 }
